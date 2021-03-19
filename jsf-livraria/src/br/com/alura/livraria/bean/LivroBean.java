ba625c38a1e188b7982291bff9b5099eeb879546
Heitor-Antunes <heitor.dlantunes@gmail.com>
2021-03-19 19:41:09 -0300

    Criando o cadastrador(LivroBean) e o modelo Livro

--- jsf-livraria/src/br/com/alura/livraria/bean/LivroBean.java
+++ jsf-livraria/src/br/com/alura/livraria/bean/LivroBean.java
@@ -1,8 +1,18 @@
 package br.com.alura.livraria.bean;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class LivroBean {
 
-	public void gravar() {
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
 	}
 	
+	public void cadastrar() {
+		System.out.println("Gravando Livro " + this.livro.getTitulo());
+	}
+
 }
